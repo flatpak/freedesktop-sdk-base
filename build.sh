4683271fbb72970157aad97fbc00fcde1177a4e7
Alexander Larsson <alexl@redhat.com>
2018-09-20 17:09:37 +0200

    Update build.sh and flathub.json
    This *should* make it possible to build this via flathub.

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@ ARCH=$1
 REPO=$2
 EXPORT_ARGS=$3
 FB_ARGS=$4
-SUBJECT=${5:-"org.freedesktop.Sdk `git rev-parse HEAD`"}
+SUBJECT=${5:-"org.freedesktop.Sdk.Base `git rev-parse HEAD`"}
 
+echo installing org.flatpak.Builder
+flatpak --user install -y flathub org.flatpak.Builder
+
+echo Building SdkBase
 flatpak run --command=make org.flatpak.Builder all ARCH="${ARCH}" REPO="${REPO}"
+
+# Remove builds, they are kind of large, and we rebuild this seldom
+echo Removing builds
+rm -rf images build
